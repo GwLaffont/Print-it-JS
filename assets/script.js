2aa8004f2f9585ccb0e98e2cb20ef2814e112b93
GLaffont <glaffont@mail.com>
2025-10-03 12:58:37 +0200

    Changement du code
    Effacé les P et img HTML
    mis en place le code de génération d'objets HTML
    Modifié le code en conséquence.

--- assets/script.js
+++ assets/script.js
@@ -1,71 +1,88 @@
-// Initial array causes error message
-const slides = [
+// Initial array 
+const slidesTest = [
 	{
-		"image":"slide1.jpg",
+		"image":"/assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"/assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"/assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"/assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ];
 
+//Creating the dots
+function dotInsert(){
+	//calculating amount of dots needed
+	//For each item in slidesTest, add 1 dot
+	for(let i = 0; i < slidesTest.length; i++){
+		//Create div with the dot class
+		//Create the variables
+		let divDots = document.createElement("div");
+		divDots.classList.add("dot");
+		let dotsContainer = document.getElementsByClassName("dots")[0];
+		//add a div.dot
+		dotsContainer.appendChild(divDots);
+	}
+}
+dotInsert();
 
+// Creating the carousel
 
+let slideIndex = 0;
 
-// Declaring Items
-
-// Nodelist containing all images within #banner
-let slidePics = document.querySelectorAll("#banner .banner-img");
-// Nodelist containing all '.dot' divs within the '.dots' div
-let listeDots = document.querySelectorAll(".dots .dot");
-// Placeholder declaration
-let slideText = document.querySelectorAll("#banner .text");
-//Declaring the Carousel Index
-let slideIndex = 1;
-//Declaring the Carousel itself: Displays the carousel image that corresponds to the slideIndex
-imgSlider(slideIndex);
-
-
-// Declaring Functions
-function imgSlider(slideIndex) {
-	//Initial i declaration
+function carousel (slideIndex){
+	//Initial I declaration
 	let i;
 
-	// Working on the pics
-	for (i = 0; i < slidePics.length; i++){
-		// Hides pics that are no longer selected
-		slidePics[i].style.display = "none";
-	};
+	//Declare target area for insertion
+	let targetArea = document.getElementById("banner");
 
-	// Working on the dots
-	for (i = 0; i < listeDots.length; i++){
-		// Removes the dot_selected class in dots that are no longer selected
-		listeDots[i].className = listeDots[i].className.replace(" dot_selected", "");
-	};
+	// Picture declaration
+	// Create the img in HTML with the banner-img class
+	let picture = document.createElement("img");
+	picture.classList.add("banner-img");
+	targetArea.appendChild(picture);
+	// Adding the src of the pic
+	let targetImg = document.getElementsByClassName("banner-img")[0];
+	let carouselPic = slidesTest[slideIndex].image;
+	targetImg.setAttribute("src", carouselPic);
 
-	// Working on the text
-	for (i = 0; i < slideText.length; i++){
-		// Hides pics that are no longer selected
-		slideText[i].style.display = "none";
-	};
-		
-	// Shows the current pic
-	slidePics[slideIndex-1].style.display = "block";
-	// adds the dot_selected class to the dot matching the current pic
-	listeDots[slideIndex-1].className += " dot_selected";
-	// Shows the current text
-	slideText[slideIndex-1].style.display = "block";
+	//Text declaration
+	//Create the paragraph 
+	let text = document.createElement("p");
+	text.classList.add("placeholder");
+	targetArea.appendChild(text);
+	//Adding the content of the text
+	let targetText = document.getElementsByClassName("placeholder")[0];
+	let carouselText = slidesTest[slideIndex].tagLine;
+	targetText.innerHTML = carouselText;
+
+	//Dot declaration
+	//Adds the dot_selected class to the dot matching the current pic
+	let listeDots = document.querySelectorAll(".dots .dot");
 	
+	//For Each dot element in listeDots
+	listeDots.forEach((dot, i) => {
+		//if any element possesses the dot_selected class, remove it
+		if (dot.classList.contains('dot_selected')) dot.classList.remove('dot_selected');
+		// Add the dot_selected class to the dot correspondant au slideIndex
+		if (i === slideIndex) dot.classList.add("dot_selected");
+	})
 };
+
+
+// Declaring the carousel
+
+carousel(slideIndex);
+
 
 //BUTTONS
 //Event listeners recommended to avoid mixing HTML and JavaScript (by using 'onclick')
@@ -77,10 +94,10 @@ let prevButton = document.getElementsByClassName("arrow_left")[0];
 prevButton.addEventListener("click", function() {
 	console.log("Previous");
 	//If slider isn't the first pic, decrease Carousel index by 1
-	if (slideIndex > 1) {
-		imgSlider(slideIndex -= 1);
+	if (slideIndex > 0) {
+		carousel(slideIndex -= 1);
 	} else {
-		imgSlider(slideIndex = slidePics.length);
+		carousel(slideIndex = slidesTest.length-1);
 	};
 	console.log(slideIndex);
 });
@@ -91,11 +108,11 @@ let nextButton = document.getElementsByClassName("arrow_right")[0];
 nextButton.addEventListener("click", function() {
 	console.log("Next");
 	//If slider isn't the last pic, increase Carousel index by 1
-	if (slideIndex < slidePics.length) {
-		imgSlider(slideIndex += 1);
+	if (slideIndex < slidesTest.length-1) {
+		carousel(slideIndex += 1);
 	} else {
-		//Otherwise, set it back to 1
-		imgSlider(slideIndex = 1);
+		//Otherwise, set it back to 0
+		carousel(slideIndex = 0);
 	};
 	console.log(slideIndex);
 });
